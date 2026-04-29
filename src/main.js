af6a1bd54d4f0cdee760e93dcfa89b378573acf6
ClickManer <slava.metelicza@bk.ru>
2026-04-29 14:35:54 +0300

    Lab10: CI/CD pipeline

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello from CI/CD!\n');
+  res.end();
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
